7a8fef5d2ea7ab6a4029476e2c0c7d15e1445794
jay <joydeepadhikari@yahoo.in>
2018-04-21 20:52:29 +0530

    first app for home control

---
+++ app/src/main/java/com/jay_adhikariyahoo/homeapp/MainActivity.java
@@ -0,0 +1,128 @@
+package com.jay_adhikariyahoo.homeapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(String... urls)
+        {
+            Log.i("URL",urls[0]);
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try
+            {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while(data != -1)
+                {
+                    char current = (char)data;
+                    result += current;
+
+                    data = reader.read();
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+    }
+
+    public void HttpRequest(String url)
+    {
+        DownloadTask lightTask = new DownloadTask();
+
+        String result=null;
+        try
+        {
+            result = lightTask.execute(url).get();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (ExecutionException e)
+        {
+            e.printStackTrace();
+        }
+        Log.i("Contents of URL", result);
+    }
+
+    public void DisplayMesg(String mesgTodisplay)
+    {
+        Toast.makeText(this, mesgTodisplay, Toast.LENGTH_SHORT).show();
+    }
+    public void micClick(View view)
+    {
+        Log.i("info","mic button pressed");
+        DisplayMesg("mic button pressed");
+    }
+    public void lightClick(View view)
+    {
+        Log.i("info","light button pressed");
+        DisplayMesg("light button pressed");
+
+        HttpRequest("http://192.168.0.103:80/rf?D=4&t=200&id=28015&on=1&channel=1");
+
+    }
+    public void fanClick(View view)
+    {
+        Log.i("info","fan button pressed");
+        DisplayMesg("fan button pressed");
+
+        HttpRequest("http://192.168.0.103:80/rf?D=4&t=200&id=28015&on=1&channel=2");
+    }
+    public void tvClick(View view)
+    {
+        Log.i("info","tv button pressed");
+        DisplayMesg("tv button pressed");
+    }
+    public void settingClick(View view)
+    {
+        Log.i("info","settings button pressed");
+        DisplayMesg("settings button pressed");
+    }
+    public void searchClick(View view)
+    {
+        EditText myTextView = (EditText) findViewById(R.id.myTextView);//creates a text view variable and gets the text from the text view
+
+        Log.i("info","search button pressed");
+        DisplayMesg(myTextView.getText().toString());
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
